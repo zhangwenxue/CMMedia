96dca9055720f3be59637ba1ed8f4442dcf60e52
zhangwenxue <1257545592@qq.com>
2018-12-11 18:38:02 +0800

    'zlgs'

--- mobile/src/main/java/com/cm/media/ui/fragment/PlayerFragment.java
+++ mobile/src/main/java/com/cm/media/ui/fragment/PlayerFragment.java
@@ -3,6 +3,7 @@ package com.cm.media.ui.fragment;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.LayoutInflater;
+import android.view.SurfaceHolder;
 import android.view.View;
 import android.view.ViewGroup;
 import androidx.annotation.NonNull;
@@ -11,18 +12,16 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProviders;
 import com.cm.media.databinding.PlayerFragmentBinding;
 import com.cm.media.viewmodel.PlayerViewModel;
-import com.shuyu.gsyvideoplayer.builder.GSYVideoOptionBuilder;
-import com.shuyu.gsyvideoplayer.listener.GSYSampleCallBack;
-import com.shuyu.gsyvideoplayer.listener.LockClickListener;
-import com.shuyu.gsyvideoplayer.utils.OrientationUtils;
+import tv.danmaku.ijk.media.player.IjkMediaPlayer;
+
+import java.io.IOException;
 
 public class PlayerFragment extends Fragment {
     private static final String TAG = "PlayerFragment";
     private static final String ARG_VIDEO_ID = "vid";
     private PlayerViewModel mViewModel;
     private PlayerFragmentBinding mBinding;
-    private OrientationUtils orientationUtils;
-
+    private IjkMediaPlayer mPlayer;
     private int mVid;
 
 
@@ -48,66 +47,54 @@ public class PlayerFragment extends Fragment {
         if (getArguments() == null) {
             return;
         }
+        initSurfaceView();
+        initPlayer();
         mVid = getArguments().getInt(ARG_VIDEO_ID);
         mViewModel = ViewModelProviders.of(this).get(PlayerViewModel.class);
         mViewModel.getPlayingUrlLiveData().observe(this, url -> {
             if (!TextUtils.isEmpty(url)) {
-                GSYVideoOptionBuilder gsyVideoOption = new GSYVideoOptionBuilder();
-                gsyVideoOption.setIsTouchWiget(true)
-                        .setRotateViewAuto(false)
-                        .setLockLand(false)
-                        .setAutoFullWithSize(true)
-                        .setShowFullAnimation(false)
-                        .setNeedLockFull(true)
-                        .setUrl(url)
-                        .setCacheWithPlay(false)
-                        .setVideoTitle("测试视频")
-                        .setVideoAllCallBack(new GSYSampleCallBack() {
-                            @Override
-                            public void onPrepared(String url, Object... objects) {
-                                super.onPrepared(url, objects);
-                                //开始播放了才能旋转和全屏
-                                orientationUtils.setEnable(true);
-                                //isPlay = true;
-                            }
-
-                            @Override
-                            public void onQuitFullscreen(String url, Object... objects) {
-                                super.onQuitFullscreen(url, objects);
-                                if (orientationUtils != null) {
-                                    orientationUtils.backToProtVideo();
-                                }
-                            }
-                        }).setLockClickListener(new LockClickListener() {
-                    @Override
-                    public void onClick(View view, boolean lock) {
-                        if (orientationUtils != null) {
-                            //配合下方的onConfigurationChanged
-                            orientationUtils.setEnable(!lock);
-                        }
-                    }
-                }).build(mBinding.detailPlayer);
+                try {
+                    mPlayer.setDataSource(url);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                mPlayer.prepareAsync();
+                mPlayer.start();
             }
         });
         mViewModel.start(mBinding.webViewContainer, mVid);
-        //外部辅助的旋转，帮助全屏
-        orientationUtils = new OrientationUtils(getActivity(), mBinding.detailPlayer);
-        //初始化不打开外部的旋转
-        orientationUtils.setEnable(false);
 
-
-        mBinding.detailPlayer.getFullscreenButton().setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //直接横屏
-                orientationUtils.resolveByClick();
-
-                //第一个true是否需要隐藏actionbar，第二个true是否需要隐藏statusbar
-                mBinding.detailPlayer.startWindowFullscreen(getActivity(), true, true);
-            }
-        });
     }
 
+    private void initPlayer() {
+        mPlayer = new IjkMediaPlayer();
+    }
+
+    private void initSurfaceView() {
+        mBinding.surface.getHolder().addCallback(mCallback2);
+    }
+
+    private SurfaceHolder.Callback2 mCallback2 = new SurfaceHolder.Callback2() {
+        @Override
+        public void surfaceRedrawNeeded(SurfaceHolder holder) {
+
+        }
+
+        @Override
+        public void surfaceCreated(SurfaceHolder holder) {
+            mPlayer.setDisplay(holder);
+        }
+
+        @Override
+        public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+        }
+
+        @Override
+        public void surfaceDestroyed(SurfaceHolder holder) {
+
+        }
+    };
 
     @Override
     public void onStop() {
