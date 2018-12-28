7a98d2821eb9d3602073fc3c134d5abd6ec1c4d5
zhangwenxue <1257545592@qq.com>
2018-12-28 23:05:02 +0800

    has-to-code-y

---
+++ mobile/src/main/java/com/cm/media/ui/fragment/VodPlayerFragment.java
@@ -0,0 +1,34 @@
+package com.cm.media.ui.fragment;
+
+import androidx.lifecycle.ViewModelProviders;
+import android.os.Bundle;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import com.cm.media.R;
+
+public class VodPlayerFragment extends Fragment {
+
+    private VodPlayerViewModel mViewModel;
+
+    public static VodPlayerFragment newInstance() {
+        return new VodPlayerFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_vod_player, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = ViewModelProviders.of(this).get(VodPlayerViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ mobile/src/main/java/com/cm/media/ui/fragment/VodPlayerViewModel.java
@@ -0,0 +1,7 @@
+package com.cm.media.ui.fragment;
+
+import androidx.lifecycle.ViewModel;
+
+public class VodPlayerViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
